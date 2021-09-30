4d5ea7ee1beb15f63fe7c244cdcee715d121c831
Praveen Davi <72861598+Praveendavi@users.noreply.github.com>
2021-09-30 12:23:43 +0530

    print hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,6 +22,7 @@ const Home = (props) => {
       {getUserListState.apiState === "success" && (
         <>
           <span>Hello</span>
+          <span>Hello</span>
           {getUserListState.data.map((item, index) => (
             <Container is_odd={index % 2}>
               <img src={item.avatar} />
